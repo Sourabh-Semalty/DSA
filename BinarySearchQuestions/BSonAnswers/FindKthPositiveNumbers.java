73d3273336c372f0ad235c64121975f24686cf42
sourabh semalty <sourabhsemalty@convertcart.com>
2026-03-03 07:10:29 +0530

    Day 25: optimal apprach kth postiive added

--- BinarySearchQuestions/BSonAnswers/FindKthPositiveNumbers.java
+++ BinarySearchQuestions/BSonAnswers/FindKthPositiveNumbers.java
@@ -19,6 +19,20 @@ public class FindKthPositiveNumbers {
         return count;
     }
 
+    public int optimalApprach(int[] arr, int k) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] - (mid + 1) < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low + k;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 2, 3, 4, 7, 11 };
         int k = 5;
